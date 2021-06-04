116dc7e8d4f6c5089ef47837d00184219f0f0a51
CatsAMillion <therealcatsamillion@gmail.com>
2021-06-03 18:25:14 -0600

    update all dependencies

--- routes/items.js
+++ routes/items.js
@@ -3,12 +3,14 @@ var router = express.Router();
 var queries = require('../db/queries')
 const knex = require('../db/knex')
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   console.log(req.body);
   knex('item')
   .then(items => {
     res.json(items)
-  })
+  }).catch((error) => {
+    next(error);
+  });
 })
 
 module.exports = router
--- routes/newItem.js
+++ routes/newItem.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var queries = require('../db/queries')
 const knex = require('../db/knex')
 
-router.post('/new-item', function(req, res) {
+router.post('/new-item', function(req, res, next) {
   queries.createItem(req.body).then(items => {
       res.json(items[0])
-    })
+    }).catch((error) => next(error))
 })
 
 module.exports = router
